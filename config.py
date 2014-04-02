80bfc6c0ffbe76e5cb0ee0added9da44ec492b57
Joshua Michael <dottru@gmail.com>
2014-04-01 23:22:35 -0700

    Updated config

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
-# database settings
-DB_USER="latitude"
-DB_PASS=""
-DB_URI="sqlite:///:memory:"
+#  =  TITLE  =
+APP_NAME  = "MyLatitude"
+
+# DB configuration
+DB_USER   = "latitude"
+DB_PASS   = None
+DB_URI    = "sqlite:///:memory:"
 SQLALCHEMY_DATABASE_URI = DB_URI
 
-# general options
-APP_NAME ="MyLatitude"
-HOST_NAME="live.chex.io"
+# Networking info
+HOST_NAME = "live.chex.io"
 
-# enable debugging
-DEBUG=True
+# Enable debugging while in development
+DEBUG     = True
 
-# fs root
-APPLICATION_ROOT="/Users/joshua/Projects/lattest/"
-
+# Project root director
+BASE_PATH = __import__('os').getcwd()
