0f63ebaa98c562161014c27c39a5064682636724
Joshua Michael <dottru@gmail.com>
2014-04-03 13:24:16 -0700

    converted to requirejs for script loading

---
+++ static/js/map.js
@@ -0,0 +1,40 @@
+require.config({
+    baseUrl: '/static/js',
+    paths:{ 'ol': 'ol/ol' },
+    shim: {
+      'ol': {
+        'exports': 'ol'
+      }
+    }
+});
+
+require(['jquery', 'ol'], function ($, ol) {
+    console.log("Map plugin loaded.");
+
+    // Asign a document ready handler to create and populate a map.
+    $( document ).ready(function(){
+        console.log("document ready fired");
+
+        div = $("#map").first();
+        div.css( "width",  "100%"  );
+        div.css( "height", "500px" );
+
+        var map = new ol.Map({
+            target: "map",
+
+            layers: [
+              new ol.layer.Tile({
+                source: new ol.source.MapQuest({layer: 'sat'})
+              }) ],
+
+            view: new ol.View2D({
+                center: ol.proj.transform(
+                  [37.41, 8.82],
+                  'EPSG:4326',
+                  'EPSG:3857'),
+                  zoom: 4
+            })
+        });
+  });
+
+})
