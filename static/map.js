5c86b97f776513b416e7c312a84e4eb2298e55cf
Joshua Michael <dottru@gmail.com>
2014-04-02 19:23:30 -0700

    Lint

--- static/map.js
+++ static/map.js
@@ -1,9 +1,7 @@
 $( document ).ready(function(){
     div = $("#map").first();
-
     div.css( "width",  "100%"  );
-    div.css( "height", "400px" );
-
+    div.css( "height", "500px" );
 
     var map = new ol.Map({
         target: "map",
@@ -17,8 +15,4 @@ $( document ).ready(function(){
           center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
           zoom: 4 })
     });
-
-    $(document).map = map;
-
-    console.log("Map init complete.");
 });
