83dddd5397eb56a88df32c3a4c1db74da61e6ea5
Joshua Michael <dottru@gmail.com>
2014-04-03 22:33:05 -0700

    Saafe config

--- config.py
+++ config.py
@@ -1,20 +1,31 @@
 ''' Configuration settings for this web application. '''
 import os
+env = lambda k: lambda e: os.environ[k] if k in os.environ else e
+
 # Title
 APP_NAME  = "MyLatitude"
 
 # DB
 DB_USER   = "latitude"
 DB_PASS   = None
-DB_URI    = "sqlite:///:memory:"
+DB_URI    = env('LATITUDE_DATABASE')("sqlite:///:memory:")
 SQLALCHEMY_DATABASE_URI = DB_URI
 
 # Host info, defaults to an env var
-HOST_NAME = os.environ['APP_HOST'] or "live.chex.io"
+HOST_NAME = env('LATITUDE_HOST')(__import__('socket').gethostname())
+
 
 # Enable debugging while in development
 DEBUG     = True
 
+SECRET_KEY = "secret"
 # Project root dir
 BASE_PATH = os.getcwd()
 STATIC_FOLDER = BASE_PATH + '/static'
+
+global GOOGLE_CLIENT_ID
+global GOOGLE_CLIENT_SECRET
+global REDIRECT_URI
+
+GOOGLE_CLIENT_ID     = env('GOOGLE_CLIENT_ID')("")
+GOOGLE_CLIENT_SECRET = env('GOOGLE_CLIENT_SECRET')("")
